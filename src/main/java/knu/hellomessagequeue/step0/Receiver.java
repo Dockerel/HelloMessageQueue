c3839e092de29ef522df335895746d9c0d21b6b8
Giheon Do <dgh0001@naver.com>
2025-09-27 17:04:30 +0900

    Initial commit

---
+++ src/main/java/knu/hellomessagequeue/step0/Receiver.java
@@ -0,0 +1,10 @@
+package knu.hellomessagequeue.step0;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+    public void receiveMessage(String message) {
+        System.out.println("[#] Received: " + message);
+    }
+}
