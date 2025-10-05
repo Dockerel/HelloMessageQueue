2c31ed4004407e8b9af07dfed1fd6319c2895753
Giheon Do <dgh0001@naver.com>
2025-10-05 17:24:57 +0900

    feat: step2, step3

---
+++ src/main/java/knu/hellomessagequeue/step3/NotificationMessage.java
@@ -0,0 +1,20 @@
+package knu.hellomessagequeue.step3;
+
+public class NotificationMessage {
+    private String message;
+
+    public NotificationMessage() {
+    }
+
+    public NotificationMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/knu/hellomessagequeue/step3/NotificationSubscriber.java
@@ -0,0 +1,24 @@
+package knu.hellomessagequeue.step3;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificationSubscriber {
+
+    public static final String CLIENT_URL = "/topic/notifications";
+
+    public final SimpMessagingTemplate simpMessagingTemplate;
+
+    public NotificationSubscriber(SimpMessagingTemplate simpMessagingTemplate) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+    }
+
+    @RabbitListener(queues = RabbitMQConfig.QUEUE_NAME)
+    public void subscriber(String message) {
+        System.out.println("Received Notification: " + message);
+
+        simpMessagingTemplate.convertAndSend(CLIENT_URL, message);
+    }
+}
