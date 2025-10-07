6fc786d60508229c0f421a3ebb561a1586d3bcfc
Giheon Do <dgh0001@naver.com>
2025-10-07 18:00:38 +0900

    feat: step9

---
+++ src/main/java/knu/hellomessagequeue/step9/StockRepository.java
@@ -0,0 +1,8 @@
+package knu.hellomessagequeue.step9;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StockRepository extends JpaRepository<StockEntity, Long> {
+}
